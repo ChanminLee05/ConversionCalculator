7e75e2c034373f6a905b457f678c0d2025d435a0
Chanmin Lee <lee00837@algonquinlive.com>
2024-08-01 21:52:32 -0600

    changed css

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import './App.css';
 import Converter from "../pages/Converter";
-import {converterType} from "../entities/ConverterType";
-
-const title = document.getElementById("title");
-const fromOption = document.getElementById("from-option");
-const toOption = document.getElementById("to-option");
-const input = document.getElementById("input");
-const convertBtn = document.getElementById("convert-btn");
-const resetBtn = document.getElementById("reset-btn");
-const swapBtn = document.getElementById("swap-btn");
 
 function App() {
   return (
